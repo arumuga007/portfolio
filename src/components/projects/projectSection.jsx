dd0ed3f1b7671765a926504a1ead8461587a1e1a
Arumugaperumal N <arumugaperumal0007@gmail.com>
2023-12-19 13:37:12 +0530

    project section ui created successfully

--- src/components/projects/projectSection.jsx
+++ src/components/projects/projectSection.jsx
@@ -4,10 +4,16 @@ import SingleProject from './singleProject';
 const ProjectSection = () => {
     const[typeOfProject, setTypeOfProject] = useState(0);
     const projectItems = [
-        {'image': 'shareNest.jpg', 'type': 1, 'title': 'ShareNest ', 'period': ' (Dec 2023 - present)', 'technologyUsed': [
-            'react', 'scss', 'express js'], 'sourceCode': 'https://github.com/arumuga007/ShareNest', 'liveDemo': false, 'description': 'ShareNest is online platform where user can either buy or sell or take any item for rent from their local community people. User can also request any item which will be further be shared as  a notification to all other users in user specified range.'},
-        {'image': 'whizocart.png', 'type': 2, 'title': 'Whizocart ', 'period': '(Aug 2023 - Sep 2023)', 'technologyUsed': [
-            'JS', 'Laravel'], 'sourceCode': false, 'liveDemo': 'https://whizocart.com', 'description': 'ShareNest is online platform where user can either buy or sell or take any item for rent from their local community people. User can also request any item which will be further be shared as  a notification to all other users in user specified range.'}
+        {'image': 'shareNest.jpg', 'type': 1, 'title': 'Share Nest ', 'period': ' (Dec 2023 - present)', 'transitionTime': '2s' ,'technologyUsed': [
+            'react', 'scss', 'express js'], 'sourceCode': 'https://github.com/arumuga007/ShareNest', 'liveDemo': false, 'description': 'Share Nest is online platform where user can either buy or sell or take any item for rent from their local community people. User can also request any item which will be further be shared as  a notification to all other users in user specified range.'},
+        {'image': 'whizocart-compressed.jpg', 'type': 2, 'title': 'Whizocart ', 'period': '(Aug 2023 - Sep 2023)', 'transitionTime': '10s' , 'technologyUsed': [
+            'JavaScript', 'Laravel'], 'sourceCode': false, 'liveDemo': 'https://whizocart.com', 'description': 'Whizocart will help to elevate your E-commerce Business to New Heights with Whizocart AI-powered solution. Experience a personalized AI solution that redefines your e-commerce journey.'},
+        {'image': 'filmFusion-compressed.jpg', 'type': 1, 'title': 'Film Fusion ', 'period': '(Oct 2023 - Nov 2023)', 'transitionTime': '2.5s' , 'technologyUsed': [
+                'React', 'Webpack'], 'sourceCode': 'https://github.com/arumuga007/film-fusion', 'liveDemo': 'https://arumuga007.github.io/film-fusion/', 'description': 'Film Fusion is a React single-page web app showcasing real-time movie data from IMDb API, featuring a modern, responsive UI for a smooth browsing experience.'},
+        {'image': 'travelNeuro-compressed.jpg', 'type': 2, 'title': 'Travel Neuro ', 'period': '(Sep 2023 - Oct 2023)', 'transitionTime': '7s' , 'technologyUsed': [
+            'Wordpress', 'Laravel'], 'liveDemo': 'https://travelneuro.com', 'description': 'Developed a feature-rich tour booking platform using WordPress, Elementor, and Laravel, enabling users to seamlessly book tours for various tourist destinations.'},
+            {'image': 'ecommerce-compressed.jpg', 'type': 1, 'title': 'E-commerce ', 'period': '(Jul 2023 - Aug 2023)', 'transitionTime': '5s' , 'technologyUsed': [
+                'JavaScript', 'Css', 'Laravel'], 'sourceCode': 'https://github.com/arumuga007/e-commerce-website-using-laravel', 'liveDemo': false, 'description': 'This e-commerce site offers a comprehensive experience for regular users and administrators. Admin functionalities encompassed product and category management, real-time delivery updates and statistical insights into website performance.'}
     ]
     return(
         <div className={styles['project-section-container']}>
--- src/components/projects/singleProject.jsx
+++ src/components/projects/singleProject.jsx
@@ -3,7 +3,7 @@ const SingleProject = (props) => {
     return(
         <div className={styles['single-project-container']}>
             <div className={styles['image-container']}>
-                <img src={process.env.PUBLIC_URL + `/images/projectItem/${props.item.image}`} />
+                <img src={process.env.PUBLIC_URL + `/images/projectItem/${props.item.image}`} style={{transition: `transform linear ${props.item.transitionTime}`}}/>
             </div>
             <div className={styles['details-container']}>
                 <div className={styles['project-title-container']}>
@@ -22,16 +22,16 @@ const SingleProject = (props) => {
                         {
                             props.item.technologyUsed.map((technology, key) => {
                                 return <div className={styles['technologyUsed-item']}>{technology}
-                                    </div>
+                                </div>
                             })
                         }
                     </div>
                 </div>
                 <div className={styles['source-deploy-container']}>
-                    {props.item.sourceCode && <a className={styles['source-code-container']}>code <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-brand-github"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
+                    {props.item.sourceCode && <a href={props.item.sourceCode} className={styles['source-code-container']}>code <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-brand-github"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
                         }
                     {
-                        props.item.liveDemo && <a className={styles['source-code-container']}>Live Demo <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-external-link"><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path><path d="M11 13l9 -9"></path><path d="M15 4h5v5"></path></svg></a>
+                        props.item.liveDemo && <a href={props.item.liveDemo} className={styles['source-code-container']}>Live Demo <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-external-link"><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path><path d="M11 13l9 -9"></path><path d="M15 4h5v5"></path></svg></a>
                     }
                 </div>
             </div>
